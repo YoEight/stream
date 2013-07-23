8296d89689768b2235cdd6bf4e134dc3c2110129
YoEight <yo.eight@gmail.com>
2013-07-24 00:16:07 +0200

    Basic resource management

--- Data/Process.hs
+++ Data/Process.hs
@@ -67,7 +67,7 @@ source :: Foldable f => f o -> Process m o
 source = foldMap yield
 
 (<~) :: Process1 a b -> Process m a -> Process m b
-Stop <~ _                     = Stop -- handle finalization of the source
+Stop <~ src                   = kill src 
 Yield o n <~ p                = Yield o (n <~ p)
 Await _ _ fb _ <~ Stop        = fb <~ Stop
 Await k recv _ _ <~ Yield a n = recv (k a) <~ n
@@ -77,22 +77,43 @@ f <~ Await r recv fb cl       = Await r ((f <~) . recv) (f <~ fb) (f <~ cl)
 m ~> ab = ab <~ m
 
 feed :: Process m a -> Sink m a -> Process m ()
-feed Stop t                   = Stop -- handle finalization of the sink
-feed src Stop                 = Stop -- handle finalization of the source
+feed Stop t                   = kill t
+feed src Stop                 = kill src
 feed (Yield o n) (Yield f nf) = Await (f o) (const $ feed n nf) Stop Stop
 feed (Await r recv fb cl) t   = Await r ((`feed` t) . recv) (feed fb t) (feed cl t)
 feed src (Await r recv fb cl) = Await r (feed src . recv) (feed src fb) (feed src cl) 
 
+drain :: Process m o -> Process m v
+drain Stop                = Stop
+drain (Yield _ n)         = drain n
+drain (Await r recv fb c) = Await r (drain . recv) (drain fb) (drain c)
+
+kill :: Process m o -> Process m v
+kill Stop          = Stop
+kill (Yield _ t)   = kill t
+kill (Await _ _ _ c) = drain c 
+
 run :: Monad m => Process m a -> m [a]
-run Stop = return []
-run (Yield o n) = liftM (o:) (run n)
+run Stop               = return []
+run (Yield o n)        = liftM (o:) (run n)
 run (Await r recv _ _) = (run . recv) =<< r
 
 run_ :: Monad m => Process m a -> m ()
 run_ = liftM (const ()) . run
 
 printer :: Show a => Sink IO a
-printer = repeatedly $ await yield (return print) 
+printer = repeatedly $ await yield (return print)
+
+resPrinter :: Show a => Sink IO a -- simulates the need of a resource
+resPrinter = resource (const printer) opening release
+  where
+    opening = print "Opening"
+    release = print "Release" 
+
+resource :: (r -> Process m o) -> m r -> m () -> Process m o
+resource k ack cl = Await ack ((<> cleaning) . k) cleaning cleaning 
+  where
+    cleaning = await (const Stop) cl
 
 test :: Process IO ()
-test = feed (source [1..10] ~> auto (+1)) printer 
+test = feed (source [1..10] ~> auto (+1)) resPrinter
