fac2350b22ca22cb603446940c3429b535f094cf
YoEight <yo.eight@gmail.com>
2014-03-03 23:57:45 +0100

    Free Resource Management

--- Data/Process/Resource.hs
+++ Data/Process/Resource.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : Data.Process.Resource
@@ -8,26 +9,92 @@
 -- Stability : provisional
 -- Portability : non-portable
 ----------------------------------------------------------------------------
-module Data.Process.Resource where
+module Data.Process.Resource (Res, resource, runResIO) where
 
-import System.IO.Error
+import Control.Exception (finally)
+import Data.Foldable (sequenceA_)
+import Data.IORef
+
+import qualified Data.IntMap.Strict as MS
 
 import Data.Process.Plan
 
--- | not enough
-resource :: IO r
-         -> (r -> IO ())
-         -> (r -> IO (Maybe o))
-         -> Plan IO o ()
-resource ack release step = onAwait go ack
+newtype ReleaseKey = ReleaseKey Int
+
+newtype Res m a
+    = Res
+      { unRes ::
+             forall x.
+             (a -> x) ->                                                      -- done
+             (forall r. m r -> (r -> x) -> x) ->                              -- run effect
+             (forall r. m r -> (r -> m ()) -> (r -> ReleaseKey -> x) -> x) -> -- allocation
+             (ReleaseKey -> x -> x) ->                                        -- release
+             x
+      }
+
+instance Monad (Res m) where
+    return a = Res $ \kd _ _ _ -> kd a
+
+    Res k >>= f =
+        Res $ \kd krun kreg krel ->
+            let done a = unRes (f a) kd krun kreg krel in
+            k done krun kreg krel
+
+allocate :: m r -> (r -> m ()) -> Res m (r, ReleaseKey)
+allocate ack rel = Res $ \kd _ kreg _ -> kreg ack rel (\r i -> kd (r, i))
+
+release :: ReleaseKey -> Res m ()
+release key = Res $ \kd _ _ krel -> krel key (kd ())
+
+onEffect :: m r -> Res m r
+onEffect rq = Res $ \kd krun _ _ -> krun rq kd
+
+runResIO :: Res IO a -> IO a
+runResIO (Res k) = do
+    ref <- newIORef MS.empty
+    let doDone :: a -> (Int -> IO a)
+        doDone a _ = return a
+
+        doEffect :: IO r
+                 -> (r -> (Int -> IO a))
+                 -> (Int -> IO a)
+        doEffect rq k i = do
+            r <- rq
+            k r i
+
+        doAlloc :: IO r
+                -> (r -> IO ())
+                -> (r -> ReleaseKey -> (Int -> IO a))
+                -> (Int -> IO a)
+        doAlloc ack kr k i = do
+            r <- ack
+            modifyIORef' ref (MS.insert i (kr r))
+            k r (ReleaseKey i) (i+1)
+
+        doRelease :: ReleaseKey
+                  -> (Int -> IO a)
+                  -> (Int -> IO a)
+        doRelease (ReleaseKey key) k i = do
+            m <- readIORef ref
+            let (opt, m2) = MS.updateLookupWithKey (\_ _ -> Nothing) key m
+            sequenceA_ opt
+            writeIORef ref m2
+            k i
+
+    finally (k doDone doEffect doAlloc doRelease $ 0) $ do
+        m <- readIORef ref
+        sequenceA_ (MS.elems m)
+
+resource :: m r
+         -> (r -> m ())
+         -> (r -> m (Maybe o))
+         -> Plan (Res m) o ()
+resource ack rel k = do
+    (r,key) <- await $ allocate ack rel
+    loop key r
   where
-    go r = onAwaitFb (go1 r) (handle r) (cleanup r)
-
-    handle r = catchIOError (step r) $ \e -> do
-        release r
-        ioError e
-
-    go1 r (Just o) = yield o >> onAwaitFb (go1 r) (handle r) (cleanup r)
-    go1 r _ = cleanup r
-
-    cleanup r = await (release r)
+    loop key r = do
+        opt <- await $ onEffect (k r)
+        case opt of
+            Nothing -> await $ release key
+            Just o  -> yield o >> loop key r
